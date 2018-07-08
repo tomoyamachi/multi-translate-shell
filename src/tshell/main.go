0ed2a1fbb2816e2ea513f65049fd6f132afafd69
Tomoya AMACHI <tomoya.amachi@gmail.com>
2018-07-08 09:36:48 +0900

    add main loop

--- src/tshell/main.go
+++ src/tshell/main.go
@@ -1,17 +1,88 @@
 package main
 
 import (
-	"fmt"
-	"os/user"
-	"tshell/editor"
+	"log"
+
+	"github.com/marcusolsson/tui-go"
+	"tshell/translator"
 )
 
+var (
+	translatee = ""
+	labels  = map[string]*tui.Label{}
+	resp *tui.Box
+)
+
+
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
+
+	languages := []translator.TranslateLanguage{
+		{"de", "en"},
+		{"fr", "en"},
+		{"ja","en"},
 	}
-	fmt.Printf("Hi %s! You can translate input languages.\n",
-		user.Username)
-	editor.Start("")
+
+	boxArray :=  []tui.Widget{}
+	for _, language := range languages {
+		// 出力するテキスト
+		label := tui.NewLabel("")
+		label.SetSizePolicy(tui.Expanding, tui.Expanding)
+		labels[language.From] =  label
+		// テキストを表示する領域
+		box := tui.NewVBox(label)
+		box.SetTitle(language.From + " <==> " + language.To)
+		box.SetBorder(true)
+		boxArray = append(boxArray, box)
+	}
+
+
+	// NewVBoxの引数がinterface型のため
+	// interfaceを配列に保存
+	// Goの仕様で、interface型の引数はinterfaceそのものを渡さなければならない
+	is := make([]tui.Widget, len(boxArray))
+	for i := range boxArray {
+		is[i] = boxArray[i]
+	}
+
+	resp = tui.NewVBox( is... )
+	resp.SetSizePolicy(tui.Expanding, tui.Preferred)
+	browser := tui.NewHBox(resp)
+	browser.SetSizePolicy(tui.Preferred, tui.Expanding)
+
+
+	inputEntry := tui.NewTextEdit()
+	inputEntry.SetText(translatee)
+	inputEntry.OnTextChanged(func(e *tui.TextEdit) {
+		translatee = e.Text()
+	})
+	inputBox := tui.NewHBox(inputEntry)
+	inputBox.SetTitle("Input English text")
+	inputBox.SetBorder(true)
+
+	// focusするbox指定
+	tui.DefaultFocusChain.Set(inputEntry)
+
+	root := tui.NewVBox(inputBox, browser)
+	theme := tui.NewTheme()
+	theme.SetStyle("box.focused.border", tui.Style{Fg: tui.ColorYellow, Bg: tui.ColorDefault})
+
+	ui, err := tui.New(root)
+	ui.SetTheme(theme)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ui.SetKeybinding("Enter", func() {
+		results := translator.Translate(translatee, languages)
+
+		for _, ts := range results {
+			labels[ts.Language].SetText(ts.Result.Export + "\n" + ts.Result.Reimport)
+		}
+	})
+
+	ui.SetKeybinding("Esc", func() { ui.Quit() })
+	ui.SetKeybinding("Ctrl+c", func() { ui.Quit() })
+
+	if err := ui.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
--- src/tshell/translator/translator.go
+++ src/tshell/translator/translator.go
@@ -3,45 +3,38 @@ package translator
 import (
 	"cloud.google.com/go/translate"
 	"context"
-	"fmt"
 	"golang.org/x/text/language"
 	"log"
 )
 
 type TranslateLanguage struct {
-	from string
-	to   string
+	From string
+	To   string
 }
 
 type TranslateTexts struct {
-	export string
-	reimport string
+	Export string
+	Reimport string
 }
 
 type TranslateResult struct {
-	language string
-	result TranslateTexts
+	Language string
+	Result TranslateTexts
 }
 
-func Translate() string {
-	languages := []TranslateLanguage{
-		{"ru", "en"},
-		{"ja", "en"},
-	}
-	text := "You can add some features to this repository."
-	return pararellTranslate(text, languages)
+func Translate(translatee string, languages []TranslateLanguage) []*TranslateResult {
+	return pararellTranslate(translatee, languages)
 }
 
-func pararellTranslate(beforeText string, languages []TranslateLanguage) string {
-	var results []TranslateResult
-
+func pararellTranslate(beforeText string, languages []TranslateLanguage) []*TranslateResult {
+	var results []*TranslateResult
 	for _, language := range languages {
-		export := requestTranslate(beforeText, language.from)
-		reimport := requestTranslate(export, language.to)
-		results = append(results, TranslateResult{language.from, TranslateTexts{export, reimport}})
+		export := requestTranslate(beforeText, language.From)
+		reimport := requestTranslate(export, language.To)
+		results = append(results, &TranslateResult{language.From, TranslateTexts{export, reimport}})
 	}
 
-	return "Sample String"
+	return results
 }
 
 func requestTranslate(text string, toLanguage string) string {
@@ -50,17 +43,13 @@ func requestTranslate(text string, toLanguage string) string {
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
-
 	target, err := language.Parse(toLanguage)
 	if err != nil {
 		log.Fatalf("Failed to parse target language: %v", err)
 	}
-
 	translations, err := client.Translate(ctx, []string{text}, target, nil)
 	if err != nil {
 		log.Fatalf("Failed to translate text: %v", err)
 	}
-
-	fmt.Printf("Translation: %v\n", translations[0].Text)
 	return translations[0].Text;
 }
